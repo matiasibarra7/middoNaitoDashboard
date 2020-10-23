d2ac06a64b666c5dec273971e1053672e623e4d3
Matias <matiasibarra7@gmail.com>
2020-10-22 21:17:28 -0300

    react last product done at 71%

--- src/components/LastProduct.js
+++ src/components/LastProduct.js
@@ -1,22 +1,55 @@
 import React from 'react';
+import { Component } from 'react'
 
-function LastProduct() {
-  return (
-    <div className="col-lg-6 mb-4">
-      <div className="card shadow mb-4">
-          <div className="card-header py-3">
-              <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
+
+class LastProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { productData: null }
+  };
+
+  setLastProduct(product) {
+    this.setState({ productData: product })
+  };
+
+
+  componentDidMount(){
+    fetch('http://localhost:3001/api/products/last')
+    .then(response => response.json())
+    .then(lastProduct => {
+
+      this.setLastProduct(lastProduct)
+      //console.log(lastProduct)
+
+    });
+  }
+
+  render() {
+    
+    /* this.state.productData? console.log(this.state.productData.imageUrl) : console.log('todavia no existe') */
+    console.log(this.state.productData)
+    return (
+      <>
+        {this.state.productData? 
+          <div className="col-lg-6 mb-4">
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h6 className="m-0 font-weight-bold text-primary">Last product in Data Dase</h6>
+                </div>
+                <div className="card-body">
+                    <div className="text-center"> 
+                      <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '25rem'}} src={this.state.productData.imageUrl} alt="imagen dummy"/>   
+                    </div>
+                      <p>{this.state.productData.description}</p>
+                      <a href={`http://localhost:3001/products/details/${this.state.productData.id}`}>View product detail</a>
+                </div>
+            </div>
           </div>
-          <div className="card-body">
-              <div className="text-center">
-                  <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: '25rem'}} src="images/product_dummy.svg" alt="imagen dummy"/>
-              </div>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa exercitationem ratione?</p>
-                  <a target="_blank" rel="nofollow" href="/">View product detail</a>
-          </div>
-      </div>
-    </div>
-  );
+          :<div>Cargando...</div>
+        }
+      </>
+    );
+  }
 }
 
 export default LastProduct;
